a509e01134488ca5062e9fdc3b4d5631200aa265
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2014-06-22 00:49:38 -0700

    login with Twitter

--- web.js
+++ web.js
@@ -1,15 +1,42 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+// requires modules and port===================================================
+var express = require('express'),
+    passport = require('passport'),
+    requestHandlers = require('./requestHandlers'),
+    port = Number(process.env.PORT || 5000);
+
+// configures app =============================================================
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.configure(function() {
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+    // configures Express
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+
+    // configures Passport
+    app.use(express.session({
+        secret: 'Cho Kim'
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // configures Passport-Twitter
+    require('./config/passport_twitter');
+
+    // configures EJS
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
 });
 
-var port = Number(process.env.PORT || 5000);
+// defines routes =============================================================
+app.get('/', requestHandlers.index);
+app.get('/auth/twitter', requestHandlers.authTwitter);
+app.get('/auth/twitter/callback', requestHandlers.authTwitterCallback);
+app.get('/profile', requestHandlers.profile);
+app.get('/logout', requestHandlers.logout);
+
+// starts server ==============================================================
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log('Listening on port ' + port);
 });
